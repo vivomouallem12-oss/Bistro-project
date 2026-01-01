d1fa88b02bbd4f761396dcadfc99ce08aa5d9a51
Victor Mouallem <vivomouallem12@gmail.com>
2026-01-01 18:35:29 +0200

    Delete ClientBistro directory

--- ClientBistro/src/gui/SubscriberChildScreen.java
+++
@@ -1,8 +0,0 @@
-package gui;
-
-import client.ClientController;
-
-public interface SubscriberChildScreen {
-    void setClient(ClientController client);
-    void setSubscriber(int subscriberId, String subscriberName);
-}
